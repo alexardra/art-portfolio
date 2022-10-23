96a24377ad7b76173f5a6210cd46158dda399391
alexardra <aurus14@freeuni.edu.ge>
2022-10-23 15:14:22 +0400

    Add project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <MainNavbar />
+  <p>
+    This will be the coolest portfolio in the future!!!
+  </p>
+  <img src="./assets/gif.png"/>
+</template>
+
+<script>
+import MainNavbar from './components/MainNavbar.vue'
+export default {
+  name: 'App',
+  components: {
+    MainNavbar
+  }
+}
+</script>
+
+<style>
+body {
+  background-color:black;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  background-color:black;
+}
+
+p {
+  margin-top: 50px;
+}
+
+img {
+  margin-top: 40px;
+  width: 400px;
+  height: 400px;
+}
+</style>
---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,31 @@
+<template>
+<ul>
+  <li>Work</li>
+  <li>Me</li>
+  <li>Starsheep</li>
+  <li>Contact</li>
+</ul>
+</template>
+
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  color: white;
+  display: flex;
+  justify-content: end;
+}
+
+ul li {
+  padding: 10px;
+  margin: 0 10px;
+  display: inline-block;
+}
+</style>
