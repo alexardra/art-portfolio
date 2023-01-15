d1a71654bcae825644aaffe64f9b2495dd4bb524
alexardra <aurus14@freeuni.edu.ge>
2023-01-15 19:01:05 +0400

    Fix webfont load strategy & preload background image

--- src/components/PageWrapperDark.vue
+++ src/components/PageWrapperDark.vue
@@ -1,27 +1,40 @@
 <template>
-  <Suspense>
-    <div class="content bg">
+<Suspense>
+  <div class="content" :style="style">
+    <slot></slot>
+  </div>
+  <template #fallback>
+    <div class="content bg-fallback">
       <slot></slot>
     </div>
-    <template #fallback>
-      <div class="content bg-fallback">
-        <slot></slot>
-      </div>
-    </template>  
-  </Suspense>
-  </template>
-  
-  <style scoped>
-  .content {
-    width: 100%;
-    height: 100%;
+  </template>  
+</Suspense>
+</template>
+
+<script>
+export default {
+  computed: {
+    imageUrl() {
+      return `${window.location.origin}/background-dark.svg`
+    },
+    style() {
+      return `background-image: url(${this.imageUrl})`
+    },
   }
-  
-  .bg-fallback {
-    background-color: #2d2d2c;
-  }
-  
-  .bg {
-    background-image: url('@/assets/background-dark.svg')
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.content {
+  width: 100%;
+  height: 100%;
+}
+
+.bg-fallback {
+  background-color: #2d2d2c;
+}
+
+/* .bg {
+  background-image: url('~/background-dark.svg')
+} */
+</style>
